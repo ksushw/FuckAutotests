ff6ba94cde205d88b7d9a5226b9713f650fbc375
Nevarenyh Kseniya <xenianevarenyh@yandex.ru>
2024-08-30 18:59:06 +0500

    feat: test for meta tags

--- cypress/e2e/existance-meta-tags/meta-tags.cy.js
+++ cypress/e2e/existance-meta-tags/meta-tags.cy.js
@@ -3,11 +3,13 @@ const aNot =
 const a =
   'http://5.45.74.202/landers/sophie_thibault_wl_la_lejournaldequebec_ca/Sophie%20Thibault_Le-Journal-de-Quebec/index.php?pwd=s712jzchsiq7hhvGxlndoz1';
 
-describe('header unclickable header links', function () {
-  it('should be disabled', function () {
-    cy.visit(
-      'http://5.45.74.202/landers/christine_lampard_wk_ka_bbc_gb/Christine-Lampard_bbc/index.php?pwd=s712jzchsiq7hhvGxlndoz1'
-    );
+describe('meta tags', function () {
+    beforeEach(() => {
+        // Посещаем страницу перед каждым тестом
+        cy.visit(a);
+    });
+  it('should contain fomat-detection tags', function () {
+    
     const expectedAttributes = [
       'telephone=no',
       'date=no',
@@ -38,5 +40,26 @@ describe('header unclickable header links', function () {
           });
       }
     });
+  });
+    
+    
+  it('Должен содержать мета-тег charset="UTF-8" либо в content, либо как отдельный атрибут', () => {
+    // Проверяем наличие мета-тега с charset в content
+      cy.get('head').then($head => {
+          if ($head.find('meta[charset="UTF-8"]').length > 0) {
+              cy.get('meta[charset="UTF-8"]').should('exist');
+
+          } else {
+              cy.get('meta[http-equiv="Content-Type"]').should('have.attr', 'content', 'text/html; charset=UTF-8');
+          }
+      });
+  });
+
+
+  it('should contain viewport tag with content "width=device-width, initial-scale=1.0"', function () {
+    cy.get('meta[name="viewport"]').should('exist');
+    cy.get('meta[name="viewport"]').should('have.attr', 'content').and((content) => {
+        expect(content).to.match(/width=device-width, initial-scale=1(\.0)?/);
+      });
   });
 });
