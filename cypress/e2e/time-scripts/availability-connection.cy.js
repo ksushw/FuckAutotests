c3c66a2d4fc2226f94a46f1600468053d2e7fe2f
Vladislav Borovinskiy <imborovinskiy@gmail.com>
2024-11-08 11:40:16 +0300

    fix time-script

--- cypress/e2e/time-scripts/availability-connection.cy.js
+++ cypress/e2e/time-scripts/availability-connection.cy.js
@@ -4,19 +4,22 @@ describe('Проверка подключения time-scripts', () => {
   it('В HTML-разметке должен присутствовать тег <script> с подключением time-scripts', () => {
     cy.visit(targetUrl);
 
-    cy.get('script[src="./time-scripts/main.js"]').should(
-      'exist',
-      'Отсутствует подключение скрипта time-scripts. Путь должен быть указан как "<script type="module" src="./time-scripts/main.js"></script>".'
-    );
+    cy.get('script[src="./time-scripts/main.js"]').then((script) => {
+      expect(
+        script.length > 0,
+        'Отсутствует подключение скрипта time-scripts. Путь должен быть указан как "<script type="module" src="./time-scripts/main.js"></script>".'
+      ).to.be.true;
+    });
   });
 
   it('В HTML-разметке должны быть атрибуты data-time-function', () => {
     cy.visit(targetUrl);
 
-    cy.get('[data-time-function]').should(
-      'have.length.at.least',
-      2,
-      'В HTML-разметке отсутсвует атрибут data-time-function'
-    );
+    cy.get('[data-time-function]').should(($elements) => {
+      expect($elements.length).to.be.at.least(
+        2,
+        'В HTML-разметке отсутствует достаточное количество атрибутов data-time-function.'
+      );
+    });
   });
 });
