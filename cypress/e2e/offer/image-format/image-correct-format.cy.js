c90008361078cb4d8af680dd6cda817642e45ff9
Vladislav Borovinskiy <imborovinskiy@gmail.com>
2025-01-23 13:52:36 +0300

    fix tests

--- cypress/e2e/offer/image-format/image-correct-format.cy.js
+++ cypress/e2e/offer/image-format/image-correct-format.cy.js
@@ -4,28 +4,37 @@ describe('Проверка форматов изображений', function ()
   it('На странице не должно быть изображений в форматах .jpg, .png, .jpeg, конвертируй в .webp', function () {
     cy.visit(targetUrl);
 
-    // Проверяем теги <img>
-    cy.get('img').each(($img) => {
-      const src = $img.attr('src');
+    // Проверяем теги <img>, если они есть
+    cy.get('img').then(($imgs) => {
+      if ($imgs.length > 0) {
+        cy.wrap($imgs).each(($img) => {
+          const src = $img.attr('src');
 
-      if (src) {
-        expect(src).not.to.match(
-          /\.(jpg|jpeg|png)(\?.*)?$/,
-          `Недопустимый формат изображения: ${src}`
-        );
+          if (src) {
+            expect(src).not.to.match(
+              /\.(jpg|jpeg|png)(\?.*)?$/,
+              `Недопустимый формат изображения: ${src}`
+            );
+          }
+        });
       }
     });
 
-    cy.get('source').each(($source) => {
-      const srcset = $source.attr('srcset');
+    // Проверяем теги <source>, если они есть
+    cy.get('source').then(($sources) => {
+      if ($sources.length > 0) {
+        cy.wrap($sources).each(($source) => {
+          const srcset = $source.attr('srcset');
 
-      if (srcset) {
-        const sources = srcset.split(',').map((src) => src.trim().split(' ')[0]);
-        sources.forEach((src) => {
-          expect(src).not.to.match(
-            /\.(jpg|jpeg|png)(\?.*)?$/,
-            `Недопустимый формат ресурса: ${src}`
-          );
+          if (srcset) {
+            const sources = srcset.split(',').map((src) => src.trim().split(' ')[0]);
+            sources.forEach((src) => {
+              expect(src).not.to.match(
+                /\.(jpg|jpeg|png)(\?.*)?$/,
+                `Недопустимый формат ресурса: ${src}`
+              );
+            });
+          }
         });
       }
     });
