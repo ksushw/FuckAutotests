810ab1a0292695286f51603c64fe802ef526712e
Vladislav Borovinskiy <imborovinskiy@gmail.com>
2025-01-23 13:42:09 +0300

    fix tests

--- cypress/e2e/offer/image-format/image-correct-format.cy.js
+++ cypress/e2e/offer/image-format/image-correct-format.cy.js
@@ -4,18 +4,28 @@ describe('Проверка форматов изображений', function ()
   it('На странице не должно быть изображений в форматах .jpg, .png, .jpeg, конвертируй в .webp', function () {
     cy.visit(targetUrl);
 
+    // Проверяем теги <img>
     cy.get('img').each(($img) => {
       const src = $img.attr('src');
 
-      expect(src).not.to.match(/\.(jpg|jpeg|png)$/, `Недопустимый формат изображения: ${src}`);
+      if (src) {
+        expect(src).not.to.match(
+          /\.(jpg|jpeg|png)(\?.*)?$/,
+          `Недопустимый формат изображения: ${src}`
+        );
+      }
     });
 
     cy.get('source').each(($source) => {
       const srcset = $source.attr('srcset');
+
       if (srcset) {
-        const sources = srcset.split(',').map((src) => src.trim());
+        const sources = srcset.split(',').map((src) => src.trim().split(' ')[0]);
         sources.forEach((src) => {
-          expect(src).not.to.match(/\.(jpg|jpeg|png)$/, `Недопустимый формат ресурса: ${src}`);
+          expect(src).not.to.match(
+            /\.(jpg|jpeg|png)(\?.*)?$/,
+            `Недопустимый формат ресурса: ${src}`
+          );
         });
       }
     });
