633043e513fdbfdbe7ff62b0f4dcd438e8390f9d
Vladislav Borovinskiy <imborovinskiy@gmail.com>
2025-01-23 14:41:05 +0300

    fix tests

--- cypress/e2e/preland/correct-link/href-correct-link.cy.js
+++ cypress/e2e/preland/correct-link/href-correct-link.cy.js
@@ -2,18 +2,29 @@ const targetUrl = Cypress.env('targetUrl');
 const expectedLink = '{{link}}';
 const specialButtonLink = '{{link}}&place=button';
 
-describe('Таги <a> с заданной ссылкой {{link}}', function () {
+describe('Проверка ссылок на странице', function () {
   it('Все ссылки, (исключая кнопки "register now"), должны иметь href="{{link}}"', function () {
     cy.visit(targetUrl);
 
-    cy.get('body a').each(($link) => {
-      const href = $link.attr('href');
+    let specialLinkCount = 0;
 
-      if ($link.text().trim() === 'register now') {
-        expect(href).to.equal(specialButtonLink);
-      } else {
-        expect(href).to.equal(expectedLink);
-      }
-    });
+    cy.get('body a')
+      .each(($link) => {
+        const href = $link.attr('href');
+
+        if (href === specialButtonLink) {
+          specialLinkCount++;
+        } else {
+          expect(href).to.equal(
+            expectedLink,
+            `Ссылка имеет некорректный href: ${href}, ожидалось ${expectedLink}`
+          );
+        }
+      })
+      .then(() => {
+        expect(specialLinkCount, 'Ссылка {{link}}&place=button должна быть только одна').to.equal(
+          1
+        );
+      });
   });
 });
