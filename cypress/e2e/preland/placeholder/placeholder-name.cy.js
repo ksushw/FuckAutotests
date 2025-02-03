3a14cb08c1f53cffb775f7403dbd4d17c46f4e94
Vladislav Borovinskiy <imborovinskiy@gmail.com>
2025-02-03 14:08:02 +0300

    fix tests

--- cypress/e2e/preland/placeholder/placeholder-name.cy.js
+++ cypress/e2e/preland/placeholder/placeholder-name.cy.js
@@ -1,15 +1,23 @@
 const targetUrl = Cypress.env('targetUrl');
 
-describe('Проверка наличия плейсхолдера {{aio.visit.fields.offer_name.for_visitor}} в HTML-разметке', function () {
+describe('Проверка корректности плейсхолдеров в HTML-разметке', function () {
   it('На странице должен быть плейсхолдер {{aio.visit.fields.offer_name.for_visitor}}', function () {
     cy.visit(targetUrl);
 
     cy.document().then((doc) => {
       const htmlContent = doc.documentElement.outerHTML;
-      const placeholder = '{{aio.visit.fields.offer_name.for_visitor}}';
+      const correctPlaceholder = '{{aio.visit.fields.offer_name.for_visitor}}';
+      const incorrectPlaceholder = '{offer_name}';
 
-      if (!htmlContent.includes(placeholder)) {
-        throw new Error(`Плейсхолдер ${placeholder} отсутствует в HTML-разметке`);
+      if (!htmlContent.includes(correctPlaceholder)) {
+        throw new Error(`Плейсхолдер ${correctPlaceholder} отсутствует в HTML-разметке`);
+      }
+
+      if (htmlContent.includes(incorrectPlaceholder)) {
+        throw new Error(
+          `В HTML-разметке найден некорректный плейсхолдер ${incorrectPlaceholder}. ` +
+            `Используйте корректный формат: ${correctPlaceholder}`
+        );
       }
     });
   });
